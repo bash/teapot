96c6402b1a2541b730b501a172d5357bdf792ebc
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-02-07 13:18:48 +0100

    Use owned string in struct

--- src/http/headers.rs
+++ src/http/headers.rs
@@ -22,7 +22,7 @@ use std::fmt;
 ///         "user-agent"
 ///     }
 ///
-///     fn parse(raw: &[RawHeader]) -> Option<Self> {
+///     fn parse(raw: &[&RawHeader]) -> Option<Self> {
 ///         if raw.len() == 0 {
 ///             return None;
 ///         }
@@ -39,26 +39,30 @@ pub trait TypedHeader: Eq + Sized {
     /// Converts a list of raw values to a `TypedHeader`
     /// The list is required for headers like `Set-Cookie` which might appear multiple times in a response.
     /// Other headers might only use the first value of `raw` and ignore the rest
-    fn parse(raw: &[RawHeader]) -> Option<Self>;
+    fn parse(raw: &[&RawHeader]) -> Option<Self>;
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct RawHeader<'a> {
-    name: &'a str,
-    value: &'a str
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub struct RawHeader {
+    name: String,
+    value: String
 }
 
-impl<'a> RawHeader<'a> {
-    pub fn new(name: &'a str, value: &'a str) -> Self {
-        RawHeader { name: name, value: value }
+impl RawHeader {
+    pub fn new<S: Into<String>>(name: S, value: S) -> Self {
+        RawHeader { name: name.into(), value: value.into() }
+    }
+
+    pub fn lower_name<'a>(&'a self) -> String {
+        self.name.to_lowercase()
     }
 
     pub fn name(&self) -> &str {
-        return &self.name
+        &self.name
     }
 
     pub fn value(&self) -> &str {
-        return &self.value
+        &self.value
     }
 }
 
@@ -72,32 +76,31 @@ impl<'a> RawHeader<'a> {
 ///
 /// assert_eq!(Dnt::Unspecified, dnt.value());
 /// ```
-pub struct Headers<'a> {
-    headers: BTreeSet<RawHeader<'a>>
+pub struct Headers {
+    headers: BTreeSet<RawHeader>
 }
 
 // TODO: allow creation from iterator
-impl<'a> Headers<'a> {
+impl Headers {
     pub fn new() -> Self {
         Headers { headers: BTreeSet::new() }
     }
 
-    pub fn append_raw(&mut self, header: RawHeader<'a>) {
+    pub fn append_raw(&mut self, header: RawHeader) {
         self.headers.insert(header);
     }
 
     pub fn get<T: TypedHeader>(&self) -> Option<T> {
         let raw = self.get_raw(T::name());
 
-        T::parse(&raw)
+        T::parse(raw.as_slice())
     }
 
-    pub fn get_raw(&self, name: &str) -> Vec<RawHeader> {
+    pub fn get_raw(&self, name: &str) -> Vec<&RawHeader> {
         // TODO: not sure if this is good
         self.headers
             .iter()
-            .filter(|ref header| *header.name().to_lowercase() == *name)
-            .map(|ref header| *header.clone())
+            .filter(|ref header| header.lower_name() == *name)
             .collect()
     }
 }
@@ -137,7 +140,7 @@ impl TypedHeader for DntHeader {
         return "dnt"
     }
 
-    fn parse(raw: &[RawHeader]) -> Option<Self> {
+    fn parse(raw: &[&RawHeader]) -> Option<Self> {
         if raw.len() == 0 {
             return Some(DntHeader { value: Dnt::Unspecified });
         }
@@ -168,7 +171,7 @@ impl TypedHeader for UserAgentHeader {
         "user-agent"
     }
 
-    fn parse(raw: &[RawHeader]) -> Option<Self> {
+    fn parse(raw: &[&RawHeader]) -> Option<Self> {
         if raw.len() == 0 {
             return None;
         }
@@ -203,7 +206,7 @@ mod test {
     #[test]
     fn test_user_agent() {
         {
-            let ua = UserAgentHeader::parse(&[RawHeader::new("user-agent", "foobar/1.1")]);
+            let ua = UserAgentHeader::parse(&[&RawHeader::new("user-agent", "foobar/1.1")]);
 
             assert!(ua.is_some());
             assert_eq!("foobar/1.1", ua.unwrap().value());
